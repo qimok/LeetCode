771690a7adca853fcb6c0229228c5ff5f6e3ed90
xinshuai <xinshuai@xingren.com>
2020-07-02 13:35:36 +0800

    378题

---
+++ LeetCode_378.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author Xinshuai
+ * @description
+ * @since 2020-07-02 13:13
+ */
+public class LeetCode_378 {
+
+    class Solution {
+
+        public int kthSmallest1(int[][] matrix, int k) {
+            int rowLen = matrix.length, columnLen = matrix[0].length;
+            int[] sorted = new int[rowLen * columnLen];
+            int index = 0;
+            for (int[] row : matrix) {
+                for (int num : row) {
+                    sorted[index++] = num;
+                }
+            }
+            Arrays.sort(sorted);
+            return sorted[k - 1];
+        }
+
+        public int kthSmallest2(int[][] matrix, int k) {
+            // 1、定义小根堆的比较函数
+            PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+            int rowLen = matrix.length;
+            // 2、把每一列的第一个元素放入小根堆
+            for (int i = 0; i < rowLen; i++) {
+                // 记录 数组（含三个元素） 数值 横坐标 纵坐标
+                pq.offer(new int[]{matrix[i][0], i, 0});
+            }
+            // 3、找到小根堆堆顶（最小值）去除，然后最小值所在的列的下一个元素入堆，一直这样操作直到找到第k小。
+            // 出栈 k-1 次 再取栈顶的时候就为第k个最小堆
+            for (int i = 0; i < k - 1; i++) {
+                int[] now = pq.poll();
+                if (now[2] != rowLen - 1) {
+                    // 把栈顶旁边的数添加到队列进行比较
+                    pq.offer(new int[]{matrix[now[1]][now[2] + 1], now[1], now[2] + 1});
+                }
+            }
+            return pq.poll()[0];
+        }
+
+    }
+
+}
