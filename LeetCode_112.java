304d97bd059097b47fdd50c99243900b15248be1
xinshuai <xinshuai@xingren.com>
2020-07-08 10:08:43 +0800

    112题

---
+++ LeetCode_112.java
@@ -0,0 +1,32 @@
+/**
+ * @author qimok
+ * @description 题目链接：https://leetcode-cn.com/problems/path-sum/solution/jian-dan-di-gui-bi-xu-miao-dong-by-sweetiee-2/
+ * @since 2020-07-08 10:03
+ */
+public class LeetCode_112 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    class Solution {
+
+        /**
+         * 递归
+         */
+        public boolean hasPathSum(TreeNode root, int sum) {
+            if (root == null) {
+                return false;
+            }
+            if (root.left == null && root.right == null) {
+                return root.val == sum;
+            }
+            return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }
+
+    }
+
+}
