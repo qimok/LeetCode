b02d02e0a46574340c0d50111be2594b4813519d
xinshuai <xinshuai@xingren.com>
2020-08-07 18:09:10 +0800

    100题

---
+++ LeetCode_100.java
@@ -0,0 +1,38 @@
+/**
+ * @author qimok
+ * @description 题目链接：https://leetcode-cn.com/problems/same-tree/
+ * @since 2020-08-07
+ */
+public class LeetCode_100 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            if (p.val != q.val) {
+                return false;
+            }
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+    }
+
+}
