38311dbb3710e76a3be877681ee97b5db0dea5ff
xinshuai <xinshuai@xingren.com>
2020-10-17 00:01:09 +0800

    279题

---
+++ LeetCode_279.java
@@ -0,0 +1,30 @@
+
+/**
+ * @author qimok
+ * @description 题目链接：https://leetcode-cn.com/problems/perfect-squares/
+ * @since 2020-10-16
+ */
+public class LeetCode_279 {
+
+    class Solution {
+
+        /**
+         * dp
+         * <p>
+         *     dp[n] 表示"和"能组成 n 的平方数的最小数量
+         *     动态转移方程：dp[i] = Math.min(dp[i], dp[i - j * j] + 1)
+         */
+        public int numSquares(int n) {
+            int[] dp = new int[n + 1]; // 默认初始化值都为 0
+            for (int i = 1; i <= n; i++) {
+                dp[i] = i; // 最坏的情况是每次都加1
+                for (int j = 1; i - j * j >= 0; j++) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+            return dp[n];
+        }
+
+    }
+
+}
