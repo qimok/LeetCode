09b6b01537b322e7b3d8bd56a00c8f429cc321b5
xinshuai <xinshuai@xingren.com>
2020-10-18 17:23:37 +0800

    518题

--- LeetCode_518.java
+++ LeetCode_518.java
@@ -7,6 +7,13 @@ public class LeetCode_518 {
 
     class Solution {
 
+        /**
+         * dp
+         * <p>
+         *     dp[i] 表示：当目标金额为 i 时的组合数
+         *     base case：dp[0] = 1
+         *     动态转移方程：dp[i] += dp[i - coin]
+         */
         public int change(int amount, int[] coins) {
             int[] dp = new int[amount + 1];
             dp[0] = 1;
