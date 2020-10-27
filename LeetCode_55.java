ebc75dc501f441d2683f1864a43648213ddee240
xinshuai <xinshuai@xingren.com>
2020-10-27 16:42:22 +0800

    55题

--- LeetCode_55.java
+++ LeetCode_55.java
@@ -1,56 +1,59 @@
 /**
  * @author qimok
- * @description 题目链接：https://leetcode-cn.com/problems/jump-game-ii/
- * @since 2020-05-04 22:27
+ * @description 题目链接：https://leetcode-cn.com/problems/jump-game/
+ * @since 2020-10-27
  */
-public class LeetCode_45 {
+public class LeetCode_55 {
 
     class Solution {
 
         /**
-         * 顺藤摸瓜
-         * <p>
-         *     时间复杂度：O(n)
-         *     空间复杂度：O(1)
+         * dp
+         * <dp>
+         *     1、状态定义：
+         *     dp[i] 表示节点 i 可达
+         *     2、状态转移：
+         *     if (dp[j] && j + nums[j] >= i) dp[i] = true
          */
-        public int jump1(int[] nums) {
-            int end = 0;
-            int maxPosition = 0;
-            int steps = 0;
-            for (int i = 0; i < nums.length - 1; i++) {
-                // 找能跳的最远的位置
-                maxPosition = Math.max(maxPosition, i + nums[i]);
-                if (i == end) {
-                    // 遇到边界，就更新边界，并且步数加一
-                    end = maxPosition;
-                    steps++;
-                }
+        public boolean canJump1(int[] nums) {
+            if (nums == null) {
+                return false;
             }
-            return steps;
-        }
-
-        /**
-         * 顺瓜摸藤
-         * <p>
-         *     时间复杂度：O(n^2)
-         *     空间复杂度：O(1)
-         */
-        public int jump2(int[] nums) {
-            // 要找的位置
-            int position = nums.length - 1;
-            int steps = 0;
-            // 是否到了第 0 个位置
-            while (position != 0) {
-                for (int i = 0; i < position; i++) {
-                    if (nums[i] >= position - i) {
-                        // 更新要找的位置
-                        position = i;
-                        steps++;
+            boolean[] dp = new boolean[nums.length];
+            dp[0] = true;
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && j + nums[j] >= i) {
+                        // 如果之前的 j 节点可达，并且从此节点可以到跳到 i，说明 i 节点可达
+                        dp[i] = true;
                         break;
                     }
                 }
             }
-            return steps;
+            return dp[nums.length - 1];
+        }
+
+        /**
+         * 贪心算法
+         * <p>
+         *     问题：设想一下，对于数组中的任意一个位置 y，我们如何判断它是否可以到达？
+         *     回答：根据题目的描述，只要存在一个位置 x，它本身可以到达，并且它跳跃的最大长度为 x + nums[x]，这个值大于等于 y，
+         *     即 x + nums[x] ≥ y，那么位置 y 也可以到达。
+         */
+        public boolean canJump2(int[] nums) {
+            int len = nums.length;
+            int rightMost = 0;
+            for (int i = 0; i < len; i++) {
+                if (i <= rightMost) {
+                    // 对于 位置i，它本身可以达到（i <= rightMost）
+                    rightMost = Math.max(rightMost, i + nums[i]);
+                }
+                if (rightMost >= len - 1) {
+                    // 如果 位置i 可以跳跃的最大长度大于等于 最后一个位置，则代表可以到达最后一个位置
+                    return true;
+                }
+            }
+            return false;
         }
 
     }
